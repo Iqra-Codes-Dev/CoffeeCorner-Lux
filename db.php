e5d464b6bf761b671579f7db107fd426ac596153
Iqra-Codes-Dev <iqra1234.official@gmail.com>
2026-07-02 01:15:38 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,6 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASSWORD');
-$dbname = getenv('DB_DATABASE');
-$port = getenv('DB_PORT');
-
-$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
-
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+$conn = mysqli_connect("localhost","root","","admin");
+if(!$conn){
+    die("Database not connected");
 }
 ?>
